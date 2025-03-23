d2d46cb28a8ffbe0a01870f1034eb98b956d2644
HariBabu T <2255095+haribabu-dev@users.noreply.github.com>
2025-03-23 16:57:07 +0800

    Fixed security group

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vp" "default" {
+data "aws_vpc" "default" {
   default =true
 }
 
@@ -56,7 +56,7 @@ resource "aws_security_group_rule" "blog_https_in" {
   security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group_rule" "blog_everythin_out" {
+resource "aws_security_group_rule" "blog_everything_out" {
   type            = "egress"
   from_port       = 0
   to_port         = 0
